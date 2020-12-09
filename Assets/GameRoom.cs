345f55b590ce91a5d12e1e71f26c36b7fe23e812
No_thoughts <57771719+TakeMe100Points@users.noreply.github.com>
2020-12-09 16:42:33 +0400

    Сделал игровую комнату, выход из комнаты

---
+++ Assets/GameRoom.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+using Photon.Realtime;
+using PN = Photon.Pun.PhotonNetwork;
+
+public class GameRoom : MonoBehaviourPunCallbacks
+{ 
+
+
+    void Start()
+    {
+        
+    }
+
+    public void Leave()
+    {
+        PN.LeaveRoom();
+    }
+
+    void Update()
+    {
+        
+    }
+
+
+    #region PN Callbacks
+    public override void OnLeftRoom()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.Log(" joined!" + newPlayer.NickName);
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        Debug.Log(" left!" + otherPlayer.NickName);
+    }
+    #endregion
+}
--- Assets/Scripts/LobbyController.cs
+++ Assets/Scripts/LobbyController.cs
@@ -1,18 +1,53 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using Photon.Pun;
+using Photon.Realtime;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using PN = Photon.Pun.PhotonNetwork;
 
-public class LobbyController : MonoBehaviour
+
+public class LobbyController : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
+    [SerializeField] private Text logger;
+
     void Start()
     {
-        
+        InitializeLobby();
     }
 
-    // Update is called once per frame
-    void Update()
+    #region PN Callbacks
+    public override void OnConnectedToMaster()
     {
-        
+        log("Connected to master server.");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        log("player joined room");
+        PN.LoadLevel(1);
+    }    
+    #endregion
+
+    public void CreateRoom()
+    {
+        PN.CreateRoom(null, new Photon.Realtime.RoomOptions { MaxPlayers = 2 });
+    }
+
+    public void JoinRandomRoom()
+    {
+        PN.JoinRandomRoom();
+    }
+
+    private void log(string lg)
+    {
+        logger.text += "\n" + lg;
+    }
+
+    private void InitializeLobby()
+    {
+        PN.GameVersion = "1";
+        PN.NickName = "player " + Random.Range(0, 1000);
+        PN.AutomaticallySyncScene = true;
+        PN.ConnectUsingSettings();
     }
 }
